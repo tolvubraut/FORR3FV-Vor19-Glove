66ff79961133bcb5ea380faf308219858a88e5e5
Robertingi00 <robertha278@nemi.tskoli.is>
2019-04-11 14:24:15 +0000

    lagfæra

--- Arduino/libraries/Flex-Sensor/flexSensor.h
+++ Arduino/libraries/Flex-Sensor/flexSensor.h
@@ -3,7 +3,6 @@
 //
 
 #include "Arduino.h"
-#include <vector>
 
 #ifndef GLOVE_FORR3FV_FLEXSENSOR_H
 #define GLOVE_FORR3FV_FLEXSENSOR_H
@@ -22,12 +21,41 @@ public:
         this->pin4 = pin4;
     };
 
-    float* getValue(){
-        float temp[4] = {analogRead(pin1),analogRead(pin2),analogRead(pin3),analogRead(pin4)};
-        float *tempPtr = &temp;
-        Serial.println(temp[0]);
-        return (tempPtr);
-    };
+
+    int FlexSensor::getValue(){
+ 
+        // # Kóðar fyrir putta
+        // handCodes = [1,2,4,8]
+            //
+        // # Values af puttunum
+        // handState = [225, 201, 104, 230]
+            //
+        // # Skilgreina hvenær putti er "niðri"
+        // limit = 128
+            //
+        // # Summa
+        // sm = 0
+            //
+        // for i in range(4):
+        //     if handState[i] > limit:
+        //         sm += handCodes[i]
+        // print(sm)
+     
+        int handCodes[4] = {1,2,4,8};
+     
+        int handState[4] = {pin1, pin2, pin3, pin4};
+     
+        float limit = 128;
+        int sm = 0;
+        for (int i = 0; i < 4; i++) {
+            if (analogRead(handState[i]) > limit) {
+                sm += handCodes[i];
+            }
+        }
+        return sm;
+    }
+
+
 };
 
 #endif //GLOVE_FORR3FV_FLEXSENSOR_H
--- Arduino/main/main.ino
+++ Arduino/main/main.ino
@@ -1,11 +1,16 @@
-#include <MPU6050_tockn.h>
-#include <Wire.h>
-#include <flexSensor.h>
+//#include <MPU6050_tockn.h>
+//#include <Wire.h>
+//#include <flexSensor.h>
 
 //MPU6050 mpu6050(Wire);
 
-FlexSensor* flexsensors = new FlexSensor(0,1,2,3);
-float *value;
+//FlexSensor* flexsensors = new FlexSensor(0,1,2,3);
+//float *value;
+
+int redPin = 11;
+int greenPin = 10;
+int bluePin = 9;
+
 
 int pin = 2;
 void setup(){
@@ -13,23 +18,48 @@ void setup(){
   /*
   Wire.begin();
   mpu6050.begin();
-  mpu6050.setGyroOffsets(1.45, 1.23, -1.32);
+  mpu6050.calcGyroOffsets(true);
   */
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);  
+  
+}
+
+void setColor(int red, int green, int blue)
+{
+  red = 255 - red;
+  green = 255 - green;
+  blue = 255 - blue;
+  analogWrite(redPin, red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue);  
 }
 
 void loop() {
+
+
+  analogWrite(redPin, 255);
+  analogWrite(greenPin, 200);
+  analogWrite(bluePin, 200); 
+  
+  
   /*
   mpu6050.update();
-  Serial.print("angleX : ");
+  Serial.println("angleX : ");
   Serial.print(mpu6050.getAngleX());
   Serial.print("\tangleY : ");
   Serial.print(mpu6050.getAngleY());
   Serial.print("\tangleZ : ");
-  Serial.println(mpu6050.getAngleZ());
-  Serial.print("Flex sensor: ");
-  */
+  Serial.print(mpu6050.getAngleZ());
+  
+
+  Serial.print("gyroAngleX : ");Serial.print(mpu6050.getGyroAngleX());
+  Serial.print("\tgyroAngleY : ");Serial.print(mpu6050.getGyroAngleY());
+  Serial.print("\tgyroAngleZ : ");Serial.println(mpu6050.getGyroAngleZ());
   Serial.print("analog: ");
   value = flexsensors->getValue();
   Serial.println(value);
-  delay(250);
+  */
+  
 }
