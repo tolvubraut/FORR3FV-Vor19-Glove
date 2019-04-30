fa2f633b8711a8a2276f0af7363ad2fc9748b2f3
Robertingi00 <robertha278@nemi.tskoli.is>
2019-04-30 09:52:57 +0000

    XD

--- Arduino/main/main.ino
+++ Arduino/main/main.ino
@@ -1,29 +1,31 @@
-#include <flexSensor.h>
+//#include <flexSensor.h>
 
 //#include <MPU6050_tockn.h>
 //#include <Wire.h>
 #include <flexSensor.h>
 
+
+
 //MPU6050 mpu6050(Wire);
 
-FlexSensor* flexsensors = new FlexSensor(0,1,2,3);
-float value;
-
+//FlexSensor* flexsensors = new FlexSensor(0,1,2,3);
+//float value;
+/*
 
 
 int pin = 2;
 void setup(){
   Serial.begin(9600);
-  /*
+  
   Wire.begin();
   mpu6050.begin();
   mpu6050.calcGyroOffsets(true);
-  */
+  
 
 }
 void loop() {
    delay(500);
-  /*
+ 
   mpu6050.update();
   Serial.println("angleX : ");
   Serial.print(mpu6050.getAngleX());
@@ -39,7 +41,31 @@ void loop() {
   Serial.print("analog: ");
   value = flexsensors->getState();
   Serial.println(value);
-  */
+  
   value = flexsensors->getState();
   Serial.println(value);
+  
+  
+*/
+RF24 radio(7, 8);  // CE, CSN
+
+//address through which two modules communicate.
+const byte address[6] = "00001";
+
+void setup() {
+  radio.begin();
+  radio.openWritingPipe(address);
+  // Power amplitude level: minimum
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
 }
+
+void loop() {
+  // Búa til data til þess að senda
+  const char text[] = "Hello World";
+  // Senda data
+  radio.write(&text, sizeof(text));
+  // Bíða 1 sec þangað til sent er aftur
+  delay(1000);
+}
+    
